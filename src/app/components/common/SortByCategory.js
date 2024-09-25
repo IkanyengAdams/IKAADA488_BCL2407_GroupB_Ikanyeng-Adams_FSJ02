7b2209e0c92c75573721acd76e033c128f324e04
Ikanyeng <ikanyengadams2@gmail.com>
2024-09-25 11:59:36 +0200

    Added default option and mapped categories to options in SortByCategory component

--- src/app/components/common/SortByCategory.js
+++ src/app/components/common/SortByCategory.js
@@ -12,7 +12,12 @@ export default function SortByCategory({ onSort }) {
           onChange={handleSort}
           className="w-full px-4 py-2 border rounded-md"
         >
-       
+          <option value="">Sort by Category</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </div>
     </div>
